e8ae1a4eb96bf5ba26a13d9918f91d2b73cd3567
rsp777 <54711053+rsp777@users.noreply.github.com>
2025-02-16 00:18:04 +0530

    Implemented InventoryWrapper for using httpclient5

--- sop-assignment-service/src/main/java/com/pawar/sop/assignment/log/wrapper/InventoryWrapper.java
+++ sop-assignment-service/src/main/java/com/pawar/sop/assignment/log/wrapper/InventoryWrapper.java
@@ -1,12 +1,29 @@
 package com.pawar.sop.assignment.log.wrapper;
 
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.pawar.inventory.entity.ASNDto;
+import com.pawar.inventory.entity.LpnDto;
+import com.pawar.sop.assignment.config.AsnServiceConfiguration;
 import com.pawar.sop.assignment.config.InventoryServiceConfiguration;
 import com.pawar.sop.assignment.httputils.HttpUtils;
 import com.pawar.sop.http.service.HttpService;
@@ -16,24 +33,30 @@ public class InventoryWrapper {
 
 	private final static Logger logger = LoggerFactory.getLogger(SopLogWrapper.class);
 
+	private final AsnServiceConfiguration asnServiceConfiguration;
 	private final InventoryServiceConfiguration inventoryServiceConfiguration;
 	private final HttpUtils httpUtils;
 	private final HttpService httpService;
+	private final ObjectMapper objectMapper;
+
 	
 //	public InventoryWrapper(InventoryServiceConfiguration inventoryServiceConfiguration, HttpUtils httpUtils) {
 //		this.inventoryServiceConfiguration = inventoryServiceConfiguration;
 //		this.httpUtils = httpUtils;
 //	}
 	
-	public InventoryWrapper(InventoryServiceConfiguration inventoryServiceConfiguration, HttpUtils httpUtils,HttpService httpService) {
+	public InventoryWrapper(AsnServiceConfiguration asnServiceConfiguration,InventoryServiceConfiguration inventoryServiceConfiguration, HttpUtils httpUtils,HttpService httpService) {
+		this.asnServiceConfiguration = asnServiceConfiguration;
 		this.inventoryServiceConfiguration = inventoryServiceConfiguration;
 		this.httpUtils = httpUtils;
 		this.httpService = httpService;
+		objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JavaTimeModule());
 	}
 	
 	public boolean checkActiveInventory(String itemName) {
 		boolean isExists = false;
-		String json = createInventoryJson(itemName);
+		String json = createInventoryJson(itemName,null);
 		logger.info("check Active Inventory Payload : " + json);
 		String url = inventoryServiceConfiguration.getCheckActiveInventoryURL();
 		logger.info("URL : {}", url);
@@ -49,11 +72,15 @@ public class InventoryWrapper {
 		return isExists;
 	}
 
-	private String createInventoryJson(String itemName) {
+	private String createInventoryJson(String itemName,String locnBrcd) {
 		JSONObject inventory_json = new JSONObject();
 		JSONObject inventory = new JSONObject();
 		JSONObject item = new JSONObject();
 		item.put("item_name", itemName);
+		if (locnBrcd!=null) {
+			item.put("location", locnBrcd);
+
+		}
 		inventory.put("item", item);
 		inventory_json.put("inventory", inventory);
 		logger.info("{}", inventory_json);
@@ -61,4 +88,30 @@ public class InventoryWrapper {
 		return json;
 	}
 
+	public void createInventory(String itemBrcd, String locnBrcd) {
+		String json = createInventoryJson(itemBrcd, locnBrcd);
+		logger.info("json ; " + json);
+		String url = inventoryServiceConfiguration.getCreateInventoryURL();
+		logger.info("CREATE_INVENTORY : {}", url);
+		String response = httpService.restCall(url, HttpMethod.POST, json, null).getBody().toString();
+		logger.info(response);		
+	}
+
+	public List<ASNDto> fetchASNs(String category) throws JsonMappingException, JsonProcessingException {
+		String url = asnServiceConfiguration.getASNByCategoryURL().replace("{category}", category);
+		logger.info("Get ASN URL : {}", url);
+		String json = httpService.restCall(url, HttpMethod.GET, null, null).getBody().toString();
+		List<ASNDto> fetchedASNDtos = objectMapper.readValue(json, new TypeReference<List<ASNDto>>() {
+		});
+		return fetchedASNDtos;
+	}
+
+	public List<LpnDto> fetchLpns(String category) throws JsonMappingException, JsonProcessingException {
+		String url = inventoryServiceConfiguration.getGetLpnByCategoryURL().replace("{category}", category);
+		logger.info("Get LPN URL : {}", url);
+		String json = httpService.restCall(url, HttpMethod.GET, null, null).getBody().toString();
+		List<LpnDto> fetchedLpnDtos = objectMapper.readValue(json, new TypeReference<List<LpnDto>>() {
+		});
+		return fetchedLpnDtos;
+	}
 }
